acfe893cc4983b0a8fcc627d85e3e3ac5b869aba
Matthieu Mourisson <mareek@gmail.com>
2016-11-12 12:16:18 +0100

    First commit

---
+++ ContinuousInterval.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intervals
+{
+    public class ContinuousInterval<T> where T : IComparable<T>
+    {
+        public ContinuousInterval(T lowerBound, T upperBound)
+        {
+            if (lowerBound == null)
+            {
+                throw new ArgumentNullException(nameof(lowerBound));
+            }
+            else if (upperBound == null)
+            {
+                throw new ArgumentNullException(nameof(upperBound));
+            }
+            else if (upperBound.CompareTo(lowerBound) < 0)
+            {
+                throw new ArgumentException($"{nameof(upperBound)} must be superior or equal to {nameof(lowerBound)}", nameof(upperBound));
+            }
+
+            LowerBound = lowerBound;
+            UpperBound = upperBound;
+        }
+
+        public T UpperBound { get; }
+
+        public T LowerBound { get; }
+    }
+}
